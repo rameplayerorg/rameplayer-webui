0f3dd0cc9dec5d8c62b17736e82ec9d832bf0ecb
Foilharp <asekapli@gmail.com>
2015-11-02 21:48:41 +0200

    Moved configuration options back and forth. Language translations, mainly for user settings page.

--- src/rameplayer/core/config.js
+++ src/rameplayer/core/config.js
@@ -13,7 +13,18 @@
             Title: 'Title',
             Length: 'Length',
             'Date': 'Date',
-            'Selected': 'Selected'
+            'Selected': 'Selected',
+            Save: 'Save',
+            SAVE_SETTINGS:'Save settings',
+            USER_SETTINGS: 'User settings',
+            SLAVE_DELAY: 'Slave delay',
+            HDMI_POWERCYCLING: 'Internal powercycle of HDMI-splitter',
+            HDMI_POWERCYCLING_INFO: 'Workaround for failed HDMI handshake.',
+            RESET_HDMI: 'Reset HDMI interface',
+            UI_LANGUAGE: 'Language of the user interface',
+            'Device IP': 'Device IP',
+            'Subnet mask': 'Subnet mask',
+            'Gateway IP': 'Gateway IP',
         });
         $translateProvider.translations('fi', {
             Settings: 'Asetukset',
@@ -21,7 +32,18 @@
             Title: 'Otsikko',
             Length: 'Pituus',
             'Date': 'Pvm',
-            'Selected': 'Valittu'
+            'Selected': 'Valittu',
+            Save: 'Tallenna',
+            SAVE_SETTINGS:'Tallenna asetukset',
+            USER_SETTINGS: 'Käyttäjäasetukset',
+            SLAVE_DELAY: 'Slave delay',
+            HDMI_POWERCYCLING: 'HDMI-jakajan uudelleenkäynnistys',
+            HDMI_POWERCYCLING_INFO: 'Workaround for failed HDMI handshake.',
+            RESET_HDMI: 'Uudelleenkäynnistä HDMI',
+            UI_LANGUAGE: 'Käyttöliittymän kieli',            
+            'Device IP': 'Koneen IP-osoite',
+            'Subnet mask': 'Aliverkon peite',
+            'Gateway IP': 'Gatewayn IP-osoite',
         });
         $translateProvider.preferredLanguage(settings.language);
         // http://angular-translate.github.io/docs/#/guide/19_security
