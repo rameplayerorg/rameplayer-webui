1c39ebe38216313a47cc56ffba2456c22c48dac0
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2015-11-22 12:24:52 +0200

    Autofocus in Save Playlist as... modal dialog

--- src/rameplayer/playlists/save-as-modal.js
+++ src/rameplayer/playlists/save-as-modal.js
@@ -5,14 +5,24 @@
         .module('rameplayer.playlists')
         .controller('SaveAsModalController', SaveAsModalController);
 
-    SaveAsModalController.$inject = ['$uibModalInstance'];
+    SaveAsModalController.$inject = ['$timeout', '$log', '$uibModalInstance'];
 
-    function SaveAsModalController($uibModalInstance) {
+    function SaveAsModalController($timeout, $log, $uibModalInstance) {
         var vm = this;
 
         vm.playlistTitle = '';
         vm.save = save;
         vm.cancel = cancel;
+
+        $uibModalInstance.opened.then(function() {
+            // focus to input field
+            // on mobile it opens keyboard mode automatically
+            // have to wait until fade in is finished before focus works
+            var delay = 500;
+            $timeout(function() {
+                $('#newPlaylistTitle').focus();
+            }, delay);
+        });
 
         function save() {
             $uibModalInstance.close(vm.playlistTitle);
