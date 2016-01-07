a2297caea2b1e3ca55a7e2c400c2c1d09dd49562
foilharp <asekapli@gmail.com>
2016-01-07 19:47:27 +0200

    DHCP server configuration UI in Admin settings NOTE: Disabled Gateway and DNS validation, so not to fail form saving in case of not connected to Internet and have nothing to set, THINK through.

--- src/rameplayer/admin/admin.js
+++ src/rameplayer/admin/admin.js
@@ -22,10 +22,14 @@
         vm.gatewayIpOcts = initOctets('ipGateway');
         vm.dnsServerIpOcts = initOctets('ipDnsPrimary');
         vm.dnsAlternativeServerIpOcts = initOctets('ipDnsSecondary');
+        vm.dhcpServerRangeStartIpOcts = initOctets('ipDhcpRangeStart');
+        vm.dhcpServerRangeEndIpOcts = initOctets('ipDhcpRangeEnd');
+        
+        vm.prefillDhcpOcts = prefillDhcpOcts;
 
         vm.saveSettings = saveSettings;
         vm.savingStatus = "loaded";
-
+        
         vm.isClusterMaster = false;
         vm.slaveIps = [];
 
@@ -48,13 +52,17 @@
 
         function saveSettings() {
             var valid = true;
-            var ipAddress, gatewayIp, dnsServerIp, dnsAlternativeServerIp, subnetMask;
+            var ipAddress, gatewayIp, dnsServerIp, dnsAlternativeServerIp, subnetMask, 
+                    dhcpRangeStart, dhcpRangeEnd, dhcpRangeValid;
             if (!vm.systemSettings.ipDhcpClient) {
                 ipAddress = validateIP(vm.deviceIpOcts);
                 gatewayIp = validateIP(vm.gatewayIpOcts);
                 dnsServerIp = validateIP(vm.dnsServerIpOcts);
                 dnsAlternativeServerIp = validateIP(vm.dnsAlternativeServerIpOcts);
                 subnetMask = validateIP(vm.subnetMaskOcts);
+                dhcpRangeStart = validateIP(vm.dhcpServerRangeStartIpOcts);
+                dhcpRangeEnd = validateIP(vm.dhcpServerRangeEndIpOcts);                
+                dhcpRangeValid = validateIpOrdering(vm.dhcpServerRangeStartIpOcts, vm.dhcpServerRangeEndIpOcts);
 
                 var invalidFields = [];
                 if (!ipAddress) {
@@ -63,6 +71,8 @@
                 if (!subnetMask) {
                     invalidFields.push('Subnet mask');
                 }
+                /* 
+                // TODO: ?? Should it be possible to leave these unset, empty or zero, in case of not connected to Internet ?? 
                 if (!gatewayIp) {
                     invalidFields.push('Gateway');
                 }
@@ -72,6 +82,27 @@
                 if (!dnsAlternativeServerIp) {
                     invalidFields.push('Secondary DNS server');
                 }
+                */
+                if(vm.systemSettings.ipDhcpServer){
+                    if (!dhcpRangeStart) {
+                        invalidFields.push('DHCP Range Start');
+                    }
+                    if (!dhcpRangeEnd) {
+                        invalidFields.push('DHCP Range End');
+                    }
+                    if (vm.dhcpServerRangeStartIpOcts[3] == 0) {
+                        // TODO: ?? Is this really illegal range value, always broadcast or always first server ?? 
+                        invalidFields.push('DHCP Range Start Not Allowed');
+                    }
+                    if (vm.dhcpServerRangeEndIpOcts[3] == 255) {
+                        // TODO: ?? Is this really illegal range value, always broadcast or always first server ?? 
+                        invalidFields.push('DHCP Range End Not Allowed');
+                    }
+                    if(!dhcpRangeValid){
+                        invalidFields.push('DHCP Range Definition');
+                    }                    
+                }
+                
                 if (invalidFields.length) {
                     toastr.error(invalidFields.join(', '), 'Invalid settings');
                     valid = false;
@@ -84,15 +115,18 @@
                     vm.systemSettings.ipGateway = gatewayIp;
                     vm.systemSettings.ipDnsPrimary = dnsServerIp;
                     vm.systemSettings.ipDnsSecondary = dnsAlternativeServerIp;
+                    if(vm.systemSettings.ipDhcpServer){
+                        vm.systemSettings.ipDhcpRangeStart = dhcpRangeStart;
+                        vm.systemSettings.ipDhcpRangeEnd = dhcpRangeEnd;
+                    }
                 }
                 vm.systemSettings.$save(function() {
                     vm.savingStatus = 'saved';
                     toastr.success('Admin settings saved.', 'Saved');
                 });
-                // TODO: move toastr info/error to save result
-                //toastr.error('saveSettings: ' + $rootScope.rameExceptions, $rootScope.rameException);
-                //throw new Error('testerror');
             }
+            //toastr.error('saveSettings: ' + $rootScope.rameExceptions, $rootScope.rameException);
+            //throw new Error('testerror');
         }
 
         // IP address string regexp
@@ -115,6 +149,26 @@
 
             return null;
         }
+        
+        function validateIpOrdering(smallerOcts, biggerOcts) {
+            var small = smallerOcts[0] * 16777216 
+                    + smallerOcts[1] * 65536 
+                    + smallerOcts[2] * 256 
+                    + smallerOcts[3]; 
+            var big = biggerOcts[0] * 16777216 
+                    + biggerOcts[1] * 65536 
+                    + biggerOcts[2] * 256 
+                    + biggerOcts[3];
+            
+            return small <= big; 
+		}
+        
+        function prefillDhcpOcts() {
+            vm.dhcpServerRangeStartIpOcts[0] = vm.dhcpServerRangeEndIpOcts[0] = vm.deviceIpOcts[0];
+            vm.dhcpServerRangeStartIpOcts[1] = vm.dhcpServerRangeEndIpOcts[1] = vm.deviceIpOcts[1];
+            vm.dhcpServerRangeStartIpOcts[2] = vm.dhcpServerRangeEndIpOcts[2] = vm.deviceIpOcts[2];
+            vm.dhcpServerRangeStartIpOcts[3] = vm.dhcpServerRangeEndIpOcts[3] = null;
+		}
 
     }
 
